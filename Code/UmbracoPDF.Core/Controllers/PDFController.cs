02378b3f22689cf87e95426cc0e119187fff8d9d
Owain <owaingdwilliams@gmail.com>
2020-05-16 16:36:08 +0100

    Adding code to Repo

---
+++ Code/UmbracoPDF.Core/Controllers/PDFController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoPDF.Core.Controllers
+{
+    
+    public class PDFController:SurfaceController
+    {
+        [System.Web.Http.HttpPost]
+        public ActionResult GeneratePDF(string fileName)
+        {
+
+            // Added Umbraco.Core reference to allow to use IsNullOrWhiteSpace helper.
+            if(fileName.IsNullOrWhiteSpace())
+            {
+                fileName = "OwainCodes.PDF";
+            }
+            
+
+            return new Rotativa.UrlAsPdf("https://www.owain.codes")
+            {
+                FileName = fileName
+            };
+
+        }
+    }
+}
